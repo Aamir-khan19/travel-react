52f290159219eceecb5f7074c3d9ed1e1fa046c0
Ravi24052003 <ravibusinessac291@gmail.com>
2024-10-08 17:19:33 +0530

    changed div height

--- src/components/home/OurTravelAgents.jsx
+++ src/components/home/OurTravelAgents.jsx
@@ -202,7 +202,7 @@ const OurTravelAgents = () => {
             {filteredAgents.length > 0 ? (
               filteredAgents.map((agent, i) => (
                 <div className="keen-slider__slide" key={i}>
-                  <div className="flex border-[1px] p-5 border-gray-600 rounded-lg relative w-full min-h-[350px] max-[600px]:min-h-[400px]">
+                  <div className="flex border-[1px] p-5 border-gray-600 rounded-lg relative w-full min-h-[380px] max-[600px]:min-h-[400px]">
                     <div className="bg-[url('Images/travelAgenciesLogo/verifiedimg.jpeg')] bg-cover bg-center w-[145px] h-[110px] mx-2 -ml-[15px] -mt-[16px]"></div>
                 
                     <div className="flex justify-between w-full">
