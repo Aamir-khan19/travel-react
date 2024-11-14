59cba95ff28fd57bb1869231c75a640777d7d0be
Ravi24052003 <ravibusinessac291@gmail.com>
2024-11-14 18:49:13 +0530

    updated laravel base url

--- conf/conf.js
+++ conf/conf.js
@@ -1,7 +1,7 @@
 const conf = {
     backendBaseUrl: String("https://travelnworld-expressjs.vercel.app"),
-    laravelBaseUrl: "http://localhost:8000"
-    // laravelBaseUrl: "https://account.mulayamsingh.in"
+    // laravelBaseUrl: "http://localhost:8000"
+    laravelBaseUrl: "https://account.mulayamsingh.in"
 }
 
 export default conf
